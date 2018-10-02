3b3520e920c44c6d895e753eb90be6fe952417ed
incCool <phr_sdu@163.com>
2018-10-02 21:59:57 +0800

    delete treeNode.cpp and .h

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
     int a[] = {1, 2, 2, 5, 3, 3};
     int *p = a;
     CountingSort nn;
-    printk();
+   
     nn.countingSort(p, 6);
     for (int i = 0; i < 6; i++)
         cout << a[i] << endl;
