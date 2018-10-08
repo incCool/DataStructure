28936ac0f5ec4f04f71b7cb5e19dbd84b17bb130
incCool <phr_sdu@163.com>
2018-10-08 10:56:16 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -3,23 +3,11 @@
 #include "hfiles/countingSortInc.h"
 #include "hfiles/test.h"
 #include "hfiles/qsort.h"
-#include "hfiles/singleton.h"
+#include "designPattern/singleton.h"
+#include "designPattern/singleton2.h"
 
 using namespace std;
 int main() {
-    int a[] = {1, 2, 2, 5, 3, 3};
-    int *p = a;
-    QuickSort qt;
-    qt.quickSort(p, 6);
-
-    for (int i = 0; i < 6; i++) {
-        cout << p[i] << ' ';
-    }
-    cout << endl;
-    swap(p[2], p[4]);
-    for (int i = 0; i < 6; i++) {
-        cout << p[i] << ' ';
-    }
 
     main01_1();
     return 0;
