1fc1ead8781e3d2365eb34eb975b35a7d4bb874c
incCool <phr_sdu@163.com>
2018-11-14 13:51:14 +0800

    add demodefine

---
+++ demo/demodefine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by inc on 2018/11/13.
+//
+
+#include "demodefine.h"
---
+++ demo/demodefineuse.h
@@ -0,0 +1,15 @@
+//
+// Created by inc on 2018/11/13.
+//
+
+#ifndef DATASTRUCTURE_DEMODEFINEUSE_H
+#define DATASTRUCTURE_DEMODEFINEUSE_H
+
+#include "demodefine.h"
+
+
+extern "C" void print(int &, int &);
+
+int main_defineuse();
+
+#endif //DATASTRUCTURE_DEMODEFINEUSE_H
